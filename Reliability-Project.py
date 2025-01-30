9818a05b39895e252cd9bbf82886dbb9d213b55e
brfraire <166643380+brfraire@users.noreply.github.com>
2025-01-30 10:59:10 -0800

    Update Reliability-Project.py

--- Reliability-Project.py
+++ Reliability-Project.py
@@ -1,1 +1,17 @@
 # test hello!
+
+
+# Import all files
+
+
+# Clean data 
+
+
+# Create a dataframe per behavior with column = initials, rows = video and values = duration of behavior
+
+
+# Create all pairwise comparisons of unique values in a list 
+
+
+
+# Create a method that outputs mean differences from a pairwise comparison of their behavior duration. 
